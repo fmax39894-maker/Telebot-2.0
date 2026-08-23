2a17fc27ed7c75985b779f2478e539accfbed94f
fmax39894-maker <fmax39894@gmail.com>
2026-08-23 22:55:25 +0530

    Update videoSites.js

--- src/data/videoSites.js
+++ src/data/videoSites.js
@@ -2,10 +2,5 @@
 // Only sources marked FOUND by the Media Source Checker
 
 module.exports = [
-      "https://mp4-cdn77.xvideos-cdn.com/17f3816b-8e67-4938-a17c-cc2febdfe486/0/video_360p.mp4?secure=Q-shUmMeo-SnJvtg-bn5RQ==,1787516372",
-    "https://mp4-cdn77.xvideos-cdn.com/17f3816b-8e67-4938-a17c-cc2febdfe486/0/video_240p.mp4?secure=SPWEBbB3ApPWXlmT5pR8lA==,1787516372",
-    "https://cdn3.x8n.online/getvideo/xxx-european-gk5llfkxzkk.mp4"
-    "https://mp4-cdn77.xvideos-cdn.com/c9547e39-2cdc-4137-813e-602c16f7589b/0/video_360p.mp4?secure=7kx-usFyicvDebC_YxzV8w==,1787516438",
-    "https://mp4-cdn77.xvideos-cdn.com/c9547e39-2cdc-4137-813e-602c16f7589b/0/video_240p.mp4?secure=mvpwV1skTTiWBIea7Fn0Hg==,1787516438",
-    "https://cdn.x8n.online/getvideo/xxx-cumshot-59h31lkjw1v.mp4"
+      "https://mp4-cdn77.xvideos-cdn.com/17f3816b-8e67-4938-a17c-cc2febdfe486/0/video_360p.mp4?secure=Q-shUmMeo-SnJvtg-bn5RQ==,1787516372"
 ];
