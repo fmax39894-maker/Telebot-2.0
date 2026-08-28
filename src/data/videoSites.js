55c47ac2ee0ce0a25f48b5b817c162fb0db25873
fmax39894-maker <fmax39894@gmail.com>
2026-08-28 11:33:15 +0530

    Delete src/data directory

--- src/data/videoSites.js
+++
@@ -1,6 +0,0 @@
-// Grand X Video Sources
-// Only sources marked FOUND by the Media Source Checker
-
-module.exports = [
-      "https://mp4-cdn77.xvideos-cdn.com/17f3816b-8e67-4938-a17c-cc2febdfe486/0/video_360p.mp4?secure=Q-shUmMeo-SnJvtg-bn5RQ==,1787516372"
-];
