3360fdbee6ed516f1d8a0c294573c0cedb91738c
fmax39894-maker <fmax39894@gmail.com>
2026-08-28 11:33:04 +0530

    Delete api directory

--- api/health.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    service: "Grand X Telegram Bot V5",
-    webhook: "/api/webhook",
-    setup: "/api/setup"
-  });
-};
